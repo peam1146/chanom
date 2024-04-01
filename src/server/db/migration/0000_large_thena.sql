1ad6893a474957af04b73f3ad2bde744213e6905
Peam <peammy2010@hotmail.com>
2024-04-01 20:32:54 +0700

    first commit

---
+++ src/server/db/migration/0000_large_thena.sql
@@ -0,0 +1,51 @@
+CREATE TABLE `chanom_account` (
+	`userId` text(255) NOT NULL,
+	`type` text(255) NOT NULL,
+	`provider` text(255) NOT NULL,
+	`providerAccountId` text(255) NOT NULL,
+	`refresh_token` text,
+	`access_token` text,
+	`expires_at` integer,
+	`token_type` text(255),
+	`scope` text(255),
+	`id_token` text,
+	`session_state` text(255),
+	PRIMARY KEY(`provider`, `providerAccountId`),
+	FOREIGN KEY (`userId`) REFERENCES `chanom_user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `chanom_post` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text(256),
+	`createdById` text(255) NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updatedAt` integer,
+	FOREIGN KEY (`createdById`) REFERENCES `chanom_user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `chanom_session` (
+	`sessionToken` text(255) PRIMARY KEY NOT NULL,
+	`userId` text(255) NOT NULL,
+	`expires` integer NOT NULL,
+	FOREIGN KEY (`userId`) REFERENCES `chanom_user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `chanom_user` (
+	`id` text(255) PRIMARY KEY NOT NULL,
+	`name` text(255),
+	`email` text(255) NOT NULL,
+	`emailVerified` integer DEFAULT CURRENT_TIMESTAMP,
+	`image` text(255)
+);
+--> statement-breakpoint
+CREATE TABLE `chanom_verificationToken` (
+	`identifier` text(255) NOT NULL,
+	`token` text(255) NOT NULL,
+	`expires` integer NOT NULL,
+	PRIMARY KEY(`identifier`, `token`)
+);
+--> statement-breakpoint
+CREATE INDEX `account_userId_idx` ON `chanom_account` (`userId`);--> statement-breakpoint
+CREATE INDEX `createdById_idx` ON `chanom_post` (`createdById`);--> statement-breakpoint
+CREATE INDEX `name_idx` ON `chanom_post` (`name`);--> statement-breakpoint
+CREATE INDEX `session_userId_idx` ON `chanom_session` (`userId`);
