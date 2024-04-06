ea9bf56432bd2d988881ff75b9501e3227289640
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2024-04-06 20:57:06 +0700

    new init

--- src/server/common/pagination.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const PaginationInput = z.object({
-  limit: z.number().min(1).max(100).nullish(),
-  cursor: z.number().nullish(), // <-- "cursor" needs to exist, but can be any type
-  direction: z.enum(["forward", "backward"]), // optional, useful for bi-directional query
-});
