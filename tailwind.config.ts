4b8f3abc6813f55995b50ec01eff64cfd8ffce0d
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2024-04-06 18:35:04 +0700

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,6 +19,15 @@ const config = {
     },
     extend: {
       colors: {
+        chanom: "#D9B49A",
+        mustard: "#FDCD79",
+        mayonnaise: "#E9DDC7",
+        cream: "#F5ECDB",
+        brown: "5A3822",
+        pink: "#FF908A",
+        orange: "#ED612C",
+        green: "#00B88E",
+        blue: "1C8BB8",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +84,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
