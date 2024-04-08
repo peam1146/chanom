f81da0835121f8b42f0129b5e0d306849a8fe2e0
Arm <armernuntiya@gmail.com>
2024-04-08 19:01:39 +0700

    fix peep box color

--- src/app/home/_components/peep/PeepBox.tsx
+++ src/app/home/_components/peep/PeepBox.tsx
@@ -4,7 +4,7 @@ import PeepRoom from "./PeepRoom";
 export default function PeepBox() {
   return (
     <div className="flex h-[308px] w-[380px] flex-col overflow-hidden rounded-lg border-2 border-brown shadow-window">
-      <div className="flex justify-between border-b-2 border-brown bg-pink px-3 py-2">
+      <div className="flex justify-between border-b-2 border-brown bg-mustard px-3 py-2">
         <div className="flex gap-2">
           <Image src={User} width={24} height={24} alt="User Icon" />
           <h1 className="h1 my-auto font-bold text-brown">Chanom Peeps</h1>
@@ -15,7 +15,6 @@ export default function PeepBox() {
         <PeepRoom name="Benz" isChating={false} />
         <PeepRoom name="Mind" isChating={false} />
         <PeepRoom name="Peam" isChating={false} />
-        <PeepRoom name="Natacha" isChating={false} />
       </div>
     </div>
   );
